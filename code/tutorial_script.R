606bb5d9d7bfecfcf318df721c182a7d64df82f2
Samesp347 <Snesparza@uncg.edu>
2025-08-21 16:11:06 -0400

    update on tutorial script

--- code/tutorial_script.R
+++ code/tutorial_script.R
@@ -2,6 +2,8 @@
 # script begins with loading packages
 # library() function would work, but pacman::p_load() allows you to load multiple packages at once
 if(!require(pacman)) install.packages("pacman")
+if(!require(tidyverse)) install.packages("tidyverse")
+
 pacman::p_load(tidyverse)
 
 
@@ -9,11 +11,6 @@ pacman::p_load(tidyverse)
 ## you can create a section label with `Ctr + Shift + R`
 
 ## write codes as you need
-x <- c(1, 3, 4)
 
 
-# another section ---------------------------------------------------------
-## fold sections with `Alt + O`
-## unfold sections with `Alt + Shift + O`
-
-y <- seq(0, 100, by = 1)
+z <- c("a", "b", "c")
